b49f4a836f9647cb67d238fb382305b96dd74c42
Ultra Ninja <ninja@engineyard.com>
2010-09-01 20:20:26 -0700

    Make it better

--- github_post_receive_hook.rb
+++ github_post_receive_hook.rb
@@ -4,52 +4,98 @@ require 'net/http'
 require 'net/https'
 
 require 'nokogiri'
-require 'ruby-debug'
 
 require 'config'
 
-raise "No user specified" if @user.nil? || @user.empty?
-raise "No token specified" if @token.nil? || @token.empty?
-raise "No repo specified" if @repo.nil? || @repo.empty?
-raise "No urls specified" if @urls.nil? || @urls.empty?
+class PostreceiveHooks
+  def self.run(user, token, repo, urls)
+    new(user, token, repo, urls).run
+  end
 
-auth = {:login => @user, :token => @token}
+  def initialize(user, token, repo, urls)
+    @user = user
+    @token = token
+    @repo = repo
+    @urls = urls
+  end
 
-def fetch(uri, req)
-  server = Net::HTTP.new uri.host, uri.port
-  server.use_ssl = uri.scheme == 'https'
-  server.verify_mode = OpenSSL::SSL::VERIFY_NONE
-  server.start {|http| http.request(req) }
+  def run
+    raise "No user specified" if @user.nil? || @user.empty?
+    raise "No token specified" if @token.nil? || @token.empty?
+    raise "No repo specified" if @repo.nil? || @repo.empty?
+    raise "No urls specified" if @urls.nil? || @urls.empty?
+
+    puts "Current: "
+    puts current_urls
+
+    puts "Required: "
+    puts required_urls
+
+    update
+
+    puts "Confirmed: "
+    puts current_urls
+  end
+
+  def current_urls
+    @current_urls ||= load_current_urls
+  end
+
+  def load_current_urls
+    req = Net::HTTP::Get.new(uri.path)
+    req.set_form_data(auth, '&')
+
+    res = fetch(req)
+
+    raise "Repo not found" unless res.code == "200"
+
+    doc = Nokogiri::HTML(res.body)
+    form = doc.at("form[action='/#{@repo}/edit/postreceive_urls']")
+    form.search("input[name='urls[]']").map {|x| x["value"]}.compact
+  end
+
+  def required_urls
+    (current_urls + @urls).uniq
+  end
+
+  def update
+    uri = URI.parse "#{admin_page}/postreceive_urls"
+    req = Net::HTTP::Post.new(uri.path)
+
+    req.set_form_data(auth + required_urls_data, '&')
+
+    res = fetch(req)
+    unless res["Location"] == "#{admin_page}?hooks=1"
+      raise "Could not update urls"
+    end
+
+    @current_urls = nil
+  end
+
+  def required_urls_data
+    required_urls.map do |url|
+      ["urls[]", url]
+    end
+  end
+
+  def uri
+    @uri ||= URI.parse(admin_page)
+  end
+
+  def admin_page
+    "https://github.com/#{@repo}/edit"
+  end
+
+  def fetch(req)
+    server = Net::HTTP.new uri.host, uri.port
+    server.use_ssl = uri.scheme == 'https'
+    server.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    server.start {|http| http.request(req) }
+  end
+
+  def auth
+    [[:login, @user], [:token, @token]]
+  end
 end
 
-admin_page = "https://github.com/#{@repo}/edit"
-uri = URI.parse(admin_page)
-req = Net::HTTP::Get.new(uri.path)
-req.set_form_data(auth, '&')
-
-res = fetch(uri, req)
-
-doc = Nokogiri::HTML(res.body)
-form = doc.at("form[action='/#{@repo}/edit/postreceive_urls']")
-urls = form.search("input[name='urls[]']").map {|x| x["value"]}.compact
-
-puts "Current: "
-puts urls
-
-urls += @urls
-urls.uniq!
-
-puts "New: "
-puts urls
-
-
-uri = URI.parse "#{admin_page}/postreceive_urls"
-req = Net::HTTP::Post.new(uri.path)
-data = auth.to_a
-urls.each do |url|
-  data << ["urls[]", url]
-end
-
-req.set_form_data(data, '&')
-
-fetch(uri, req)
+PostreceiveHooks.run(@user, @token, @repo, @urls)
